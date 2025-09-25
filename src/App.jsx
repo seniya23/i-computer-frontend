bed50a10ff888d004a3d4c929b8cdc3613f27abc
Seniya_Lenora <seniya23>
2025-09-25 17:19:32 +0530

    D10✏️

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,23 @@ function App() {
 
   return (
     <>
-      <ProductCard name="Laptop" price="100,000.00" image="https://techterms.com/img/xl/laptop_586.png"/> 
+      <ProductCard 
+      name="Laptop" 
+      price="100,000.00" 
+      image="https://techterms.com/img/xl/laptop_586.png"
+      /> 
 
-      <ProductCard name="Monitor" price="80,000.00" image="https://techterms.com/img/xl/laptop_586.png"/>
+      <ProductCard 
+      name="Monitor" 
+      price="80,000.00" 
+      image="https://techterms.com/img/xl/laptop_586.png"
+      />
 
-      <ProductCard name="Keyboard" price="10,000.00" image="https://techterms.com/img/xl/laptop_586.png"/>
+      <ProductCard 
+      name="Keyboard" 
+      price="10,000.00" 
+      image="https://techterms.com/img/xl/laptop_586.png"
+      />
     </>
   )
 }
